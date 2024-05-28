c663985bdd469a88f9cab51d6cb8f58c32eecea2
syeda <sbtshakeel61@gmail.com>
2024-05-28 23:44:52 +0500

    Updated

--- index.ts
+++ index.ts
@@ -6,6 +6,6 @@ let tuples : [string, boolean, number] = ["sabahat", false, 20] //ok
 
 let tuples1 : [string, boolean, number, undefined, null] = ["Hello", undefined] //error
 
-//because type 'undefined' is not assinabe to type boolean
+//because type 'undefined' is not assignable to type 'boolean'.
 
 tuples.push(404) //ok
